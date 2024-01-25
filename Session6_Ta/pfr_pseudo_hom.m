28216bbd247afccb00c0450ff10d6ee6082037c7
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-01-25 14:53:12 +0100

    Update pfr_pseudo_hom.m

--- Session6_Ta/pfr_pseudo_hom.m
+++ Session6_Ta/pfr_pseudo_hom.m
@@ -1,8 +1,18 @@
-function pfr_pseudo_hom = pfr_pseudo_hom()
+function pfr_pseudo_hom = pfr_pseudo_hom(t,y)
 global Stoichiometry deltaH mw SpecificHeatP Viscosity ...
     TubeDiameter CatalystDensity U ...
 
+for i=1:nsidedpoly
+    mol(i) = y(i)/mw(i);
+end
 
+MolarFraction = mol./sum(mol);
+
+Temperature = y(end-1);
+Pressure_bar = y(end);
+Pressure_Pa = Pressure_bar* 1e5;
+
+DensityMolarGas = 1e-3 * Pressure_Pa/8.314/Temperature;  %kmol/m3
 
 
 end
