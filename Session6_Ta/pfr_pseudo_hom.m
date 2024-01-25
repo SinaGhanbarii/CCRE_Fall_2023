0460177b8d3d027485cc70d446305f55ac97d2db
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-01-25 14:56:41 +0100

    Update pfr_pseudo_hom.m

--- Session6_Ta/pfr_pseudo_hom.m
+++ Session6_Ta/pfr_pseudo_hom.m
@@ -13,6 +13,11 @@ Pressure_bar = y(end);
 Pressure_Pa = Pressure_bar* 1e5;
 
 DensityMolarGas = 1e-3 * Pressure_Pa/8.314/Temperature;  %kmol/m3
+mw_av = 0;
+
+for i = 1:nsidedpoly
+    mw_av = mw_av + MolarFraction(i)*mw(i);
+end
 
 
 end
