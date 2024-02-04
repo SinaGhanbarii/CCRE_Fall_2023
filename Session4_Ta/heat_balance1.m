950aab4df9735e5bfd1069c0a295d59e5bb0d13f
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-02-04 18:55:07 +0100

    Evening change!

--- Session4_Ta/heat_balance1.m
+++ Session4_Ta/heat_balance1.m
@@ -1,34 +1,31 @@
-function F = heat_balance1(y)
-
+function F=heat_balance1(y)
     global V c0 c_0 Q rho cp delH_rxn T_feed nu UA k0 Tcool
-
-    T = y(1);
-    c = [y(2) y(3)];
+    T=y(1);
+    c=[y(2) y(3)];
     
-    k = k0 * exp(-12000/T); %this is provided to us in the problem text 
+    k=k0*exp(-12000/T);
     
-    %this is the heat balance equation for the CSTR 
-    F(1) = ((Q*c0*-delH_rxn)/(Q/(k*V)+1)) - (Q*rho*cp*(T-T_feed)) - UA*(T-Tcool); 
+    F(1)=((Q*c0*delH_rxn)/( Q/(k*V)+1))-(Q*rho*cp*(T-T_feed))-UA*(T-Tcool);%-5000*(T-280);
     
-    c(1) = max(c(1),1e-6); %to ensure c does not become negative during solving
+    c(1)=max(c(1),1e-6);
+    
     
     %reaction rate
-    r(1) = k*c(1);
     
-    R = zeros(2,1);
+    r(1)=k*c(1);
+    
+    R=zeros(2,1);
+    
     
     for i=1:2
-        R(i) = 0;
-        R(i) = R(i) + nu(i)*r ; 
+        R(i)=0;
+        R(i)=R(i)+nu(i)*r;
     end
     
-    %these are the species balance equations for the reactor 
     for i=1:2
-        F(i+1) = Q*c_0(i) - Q*c(i) + R(i)*V ;
-    end 
-end
-    
-    
+        F(i+1)=Q*c_0(i)-Q*c(i)+R(i)*V;
+    end
+    end
     
     
     
