f17ac64f6e2c9977f139f8bf3281c6d8e80d8c67
MohammadSina Ghanbari <sina_ghanbari@outlook.com>
2024-02-04 19:25:08 +0100

    Change!

--- Session4_Ta/heat_balance1.m
+++ Session4_Ta/heat_balance1.m
@@ -4,9 +4,11 @@ function F=heat_balance1(y)
     c=[y(2) y(3)];
     
     k=k0*exp(-12000/T);
-    
-    F(1)=((Q*c0*delH_rxn)/( Q/(k*V)+1))-(Q*rho*cp*(T-T_feed))-UA*(T-Tcool);%-5000*(T-280);
-    
+    %Part 1
+    F(1)=((Q*c0*delH_rxn)/( Q/(k*V)+1))-(Q*rho*cp*(T-T_feed))-UA*(T-Tcool)-5000*(T-280);
+    %Part 2
+    %F(1)=((Q*c0*delH_rxn)/( Q/(k*V)+1))-(Q*rho*cp*(T-T_feed))-UA*(T-Tcool);%-5000*(T-280);
+
     c(1)=max(c(1),1e-6);
     
     
